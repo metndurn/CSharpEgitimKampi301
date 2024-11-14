d64dd1bf365a9073c5abadea70acc3e6183f455d
metndurn <mtndrn123@gmail.com>
2024-11-14 21:14:59 +0300

    bütün katmanlar eklendi gerekli klasör yapısı olusturuldu

---
+++ CSharpEgitimKampi301.DataAccsessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccsessLayer.Abstract
+{
+	public interface IGenericDal<T> where T : class
+	{
+		/*standart olarak buradakı metodlar kullanılacak*/
+		void Insert(T entity);
+		void Update(T entity);
+		void Delete(int id);
+		List<T> GetAll();
+		T GetById(int id);
+	}
+}
