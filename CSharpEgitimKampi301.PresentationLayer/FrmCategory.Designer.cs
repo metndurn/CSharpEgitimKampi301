c27997251604bd25cc6ab30e8927ca6e59240f92
metndurn <mtndrn123@gmail.com>
2024-12-05 00:31:47 +0300

    şu ana kadar gerekli interfaceler yazıldı ve örnek olması için form tasarımı yapıldı

---
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace CSharpEgitimKampi301.PresentationLayer
+{
+	partial class FrmCategory
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnList = new System.Windows.Forms.Button();
+			this.txtCategoryId = new System.Windows.Forms.TextBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.txtCategoryName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.rdbActive = new System.Windows.Forms.RadioButton();
+			this.rdbPassive = new System.Windows.Forms.RadioButton();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnDelete = new System.Windows.Forms.Button();
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.btnGetById = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.Location = new System.Drawing.Point(46, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(110, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Kategori ID:";
+			// 
+			// btnList
+			// 
+			this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnList.Location = new System.Drawing.Point(50, 194);
+			this.btnList.Name = "btnList";
+			this.btnList.Size = new System.Drawing.Size(263, 46);
+			this.btnList.TabIndex = 1;
+			this.btnList.Text = "Listele";
+			this.btnList.UseVisualStyleBackColor = true;
+			// 
+			// txtCategoryId
+			// 
+			this.txtCategoryId.Location = new System.Drawing.Point(162, 21);
+			this.txtCategoryId.Multiline = true;
+			this.txtCategoryId.Name = "txtCategoryId";
+			this.txtCategoryId.Size = new System.Drawing.Size(151, 29);
+			this.txtCategoryId.TabIndex = 2;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(335, 21);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(691, 469);
+			this.dataGridView1.TabIndex = 3;
+			// 
+			// txtCategoryName
+			// 
+			this.txtCategoryName.Location = new System.Drawing.Point(162, 70);
+			this.txtCategoryName.Multiline = true;
+			this.txtCategoryName.Name = "txtCategoryName";
+			this.txtCategoryName.Size = new System.Drawing.Size(151, 29);
+			this.txtCategoryName.TabIndex = 5;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.Location = new System.Drawing.Point(38, 79);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(118, 20);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Kategori Adı:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.Location = new System.Drawing.Point(9, 132);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(147, 20);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Kategori Durum:";
+			// 
+			// rdbActive
+			// 
+			this.rdbActive.AutoSize = true;
+			this.rdbActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.rdbActive.Location = new System.Drawing.Point(166, 132);
+			this.rdbActive.Name = "rdbActive";
+			this.rdbActive.Size = new System.Drawing.Size(68, 24);
+			this.rdbActive.TabIndex = 7;
+			this.rdbActive.TabStop = true;
+			this.rdbActive.Text = "Aktif";
+			this.rdbActive.UseVisualStyleBackColor = true;
+			// 
+			// rdbPassive
+			// 
+			this.rdbPassive.AutoSize = true;
+			this.rdbPassive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.rdbPassive.Location = new System.Drawing.Point(240, 132);
+			this.rdbPassive.Name = "rdbPassive";
+			this.rdbPassive.Size = new System.Drawing.Size(73, 24);
+			this.rdbPassive.TabIndex = 8;
+			this.rdbPassive.TabStop = true;
+			this.rdbPassive.Text = "Pasif";
+			this.rdbPassive.UseVisualStyleBackColor = true;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnAdd.Location = new System.Drawing.Point(50, 256);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(263, 46);
+			this.btnAdd.TabIndex = 9;
+			this.btnAdd.Text = "Ekle";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnDelete.Location = new System.Drawing.Point(50, 317);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(263, 46);
+			this.btnDelete.TabIndex = 10;
+			this.btnDelete.Text = "Sil";
+			this.btnDelete.UseVisualStyleBackColor = true;
+			// 
+			// btnUpdate
+			// 
+			this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnUpdate.Location = new System.Drawing.Point(50, 380);
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.Size = new System.Drawing.Size(263, 46);
+			this.btnUpdate.TabIndex = 11;
+			this.btnUpdate.Text = "Güncelle";
+			this.btnUpdate.UseVisualStyleBackColor = true;
+			// 
+			// btnGetById
+			// 
+			this.btnGetById.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnGetById.Location = new System.Drawing.Point(50, 444);
+			this.btnGetById.Name = "btnGetById";
+			this.btnGetById.Size = new System.Drawing.Size(263, 46);
+			this.btnGetById.TabIndex = 12;
+			this.btnGetById.Text = "ID\'ye Göre Getir";
+			this.btnGetById.UseVisualStyleBackColor = true;
+			// 
+			// FrmCategory
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1043, 508);
+			this.Controls.Add(this.btnGetById);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.rdbPassive);
+			this.Controls.Add(this.rdbActive);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtCategoryName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.txtCategoryId);
+			this.Controls.Add(this.btnList);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "FrmCategory";
+			this.Text = "Kategori Sayfası";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnList;
+		private System.Windows.Forms.TextBox txtCategoryId;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.TextBox txtCategoryName;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.RadioButton rdbActive;
+		private System.Windows.Forms.RadioButton rdbPassive;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnDelete;
+		private System.Windows.Forms.Button btnUpdate;
+		private System.Windows.Forms.Button btnGetById;
+	}
+}
+
